369d06d2c2c56939b98e0c8a11bd0a1c16a7c9b3
Franco Exequiel Salas <franco_10_65@hotmail.com>
2023-07-24 21:35:41 -0300

    filtro buscador listo

--- js/buscarCursos.js
+++ js/buscarCursos.js
@@ -1,17 +1,9 @@
-//  // Función para generar un número aleatorio entre min y max (ambos incluidos)
-//  function getRandomNumber(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-//   }
+function buscarCursos(carrera) {
 
-  // Función de búsqueda de cursos
-  function buscarCursos(curso, carrera) {
-    // Obtener la cadena JSON del localStorage
     const cursosJSON = localStorage.getItem("cursos");
-    
-    // Convertir la cadena JSON nuevamente a un objeto
+
     const cursos = JSON.parse(cursosJSON);
-    
-    curso = curso.toLowerCase();
+
     carrera = carrera.toLowerCase();
     return Object.values(cursos).filter((curso) => {
         const nombreCurso = curso.nombreCurso.toLowerCase();
@@ -22,49 +14,49 @@
         (carrera === "todas" || carreraCurso === carrera)
         );
     });
-    
-    }
-    
-      // Función para actualizar la tabla con los resultados de la búsqueda
-    function actualizarTabla(cursosFiltrados) {
+
+}
+
+
+function actualizarTabla(cursosFiltrados) {
     const cuerpoTabla = document.getElementById("cuerpoTabla");
     cuerpoTabla.innerHTML = "";
-    
-        cursosFiltrados.forEach((curso) => {
-        const fila = document.createElement("tr");
-        fila.innerHTML = `
-            <td>${curso.nombreCurso}</td>
-            <td>${curso.carrera}</td>
-            <td>${curso.duracionEnHoras} Hs.</td>
-        `;
-        cuerpoTabla.appendChild(fila);
-        });
-    
-    }
-    
-      // Función para manejar el evento de clic en el botón "Buscar"
-    // function filtraCursos() {
-    //     const inputCurso = document.getElementById("curso-form").value;
-    //     const selectCarrera = document.getElementById("carrera-form");
-    //     const carreraSeleccionada = selectCarrera.options[selectCarrera.selectedIndex].text;
-    
-    //     const cursosFiltrados = buscarCursos(inputCurso, carreraSeleccionada);
-    //     actualizarTabla(cursosFiltrados);
-    // }
-    
-    function filtraCursos() {
-        const inputCurso = document.getElementById("curso-form").value;
-        const selectCarrera = document.getElementById("carrera-form");
-        const carreraSeleccionada = selectCarrera.options[selectCarrera.selectedIndex].text;
-    
-    
-        const cursosFiltrados = buscarCursos(inputCurso, carreraSeleccionada);
-        console.log(`${cursosFiltrados}`)
-        actualizarTabla(cursosFiltrados);
-    }
-    
-      // Obtener referencias al botón "Buscar"
-      const btnBuscar = document.getElementById("nuestroCursos-btn");
-    
-      // Asignar el evento de clic al botón "Buscar"
-      btnBuscar.addEventListener("click", filtraCursos);
+    const tituloTabla = document.getElementById("tabla-titulo");
+    tituloTabla.classList.remove("d-none");
+    cursosFiltrados.forEach((curso) => {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+        <td>${curso.nombreCurso}</td>
+        <td>${curso.carrera}</td>
+        <td>${curso.duracionEnHoras} Hs.</td>
+    `;
+    cuerpoTabla.appendChild(fila);
+    });
+
+
+}
+
+function filtraCursos() {
+
+    const selectCarrera = document.getElementById("carrera-form");
+    const carreraSeleccionada = selectCarrera.options[selectCarrera.selectedIndex].text;
+
+
+    const cursosFiltrados = buscarCursos(carreraSeleccionada);
+    actualizarTabla(cursosFiltrados);
+    setTimeout(limpiarTabla, 8000);
+}
+
+function limpiarTabla() {
+    const cuerpoTabla = document.getElementById("cuerpoTabla");
+    cuerpoTabla.innerHTML = "";
+    const tituloTabla = document.getElementById("tabla-titulo");
+    tituloTabla.classList.add("d-none");
+
+}
+
+
+const btnBuscar = document.getElementById("nuestroCursos-btn");
+
+
+btnBuscar.addEventListener("click", filtraCursos);
